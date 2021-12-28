3944035b08037d939d876303dea364df67df2df0
FlutLab.io <>
2021-12-28 12:46:33 +0000

    Autosave

--- lib/widget/SizableRow.dart
+++ lib/widget/SizableRow.dart
@@ -71,7 +71,7 @@ class _SizableRowState extends State<SizableRow> {
 
                 double dx = currentGlobalPosition.dx - oldGlobalPosition!.dx;
                 if (threadhold < dx && dx < totalWidth - threadhold) {
-                  print("leftWidth:$leftWidth, dx:$dx, ");
+                  print("leftWidth:$leftWidth, dx:$dx,  oldGlobalPosition:$oldGlobalPosition,  currentGlobalPosition:$currentGlobalPosition, ");
                   leftWidth += dx;
                 }
                 setState(() {});
