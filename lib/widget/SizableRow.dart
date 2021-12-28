a4f973ee16d5973e411fc70adf8b0ebee23c4d5e
FlutLab.io <>
2021-12-28 08:29:38 +0000

    Autosave

--- lib/widget/SizableRow.dart
+++ lib/widget/SizableRow.dart
@@ -20,7 +20,7 @@ class _SizableRowState extends State<SizableRow> {
             child: Container(
               height: double.infinity,
               color: Colors.blue,
-              child: Text("LeftPage3"),
+              child: Text("LeftPage4"),
             ),
           ),
           GestureDetector(
