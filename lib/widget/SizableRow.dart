ea8924091c55a0d69ef005da0d0ab0a0e7f63f05
FlutLab.io <>
2021-12-28 11:32:13 +0000

    Autosave

--- lib/widget/SizableRow.dart
+++ lib/widget/SizableRow.dart
@@ -52,7 +52,6 @@ class _SizableRowState extends State<SizableRow> {
       children: [
         Container(
           key: widgetKey,
-          width: totalWidth,
           child: Row(
             children: [
               Expanded(
